60f8fcea27f6810f2a6ca85689a03f5beadaa194
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2023-12-19 09:51:56 +0600

    cloned: sidebar and users layout

---
+++ app/actions/getConversationById.ts
@@ -0,0 +1,28 @@
+import prismaDB from "../libs/prismadb";
+import getCurrentUser from "./getCurrentUser";
+
+const getConversationById = async (conversationId: string) => {
+    try {
+        const currentUser = await getCurrentUser();
+
+        if (!currentUser) return null;
+
+        const conversation = await prismaDB.conversation.findUnique({
+            where: {
+                id: conversationId,
+            },
+            include: {
+                users: true,
+            },
+        });
+
+        if (!conversation) return null;
+
+        return conversation;
+    } catch (error: any) {
+        console.log("[getConversationById_Error]", error);
+        return null;
+    }
+};
+
+export default getConversationById;
---
+++ app/actions/getMessages.ts
@@ -0,0 +1,25 @@
+import prismaDB from "../libs/prismadb";
+
+const getMessages = async (conversationId: string) => {
+  try {
+    const messages = await prismaDB.message.findMany({
+      where: {
+        conversationId: conversationId,
+      },
+      include: {
+        seen: true,
+        sender: true,
+      },
+      orderBy: {
+        createdAt: "asc",
+      },
+    });
+
+    return messages;
+  } catch (error: any) {
+    console.log("[getMessages_Error]", error);
+    return [];
+  }
+};
+
+export default getMessages;
