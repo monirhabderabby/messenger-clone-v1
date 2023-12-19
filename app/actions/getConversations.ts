6c170bb6a70369836c9e677732306de994794011
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2023-12-19 10:08:42 +0600

    fsd

--- app/actions/getConversations.ts
+++ app/actions/getConversations.ts
@@ -1,38 +1,38 @@
-import prisma from "@/app/libs/prismadb";
+import prisma from "@/app/lib/prismadb";
 import getCurrentUser from "./getCurrentUser";
 
 const getConversations = async () => {
-    const currentUser = await getCurrentUser();
+  const currentUser = await getCurrentUser();
 
-    if (!currentUser?.id) {
-        return [];
-    }
+  if (!currentUser?.id) {
+    return [];
+  }
 
-    try {
-        const conversations = await prisma.conversation.findMany({
-            orderBy: {
-                lastMessageAt: "desc",
-            },
-            where: {
-                userIds: {
-                    has: currentUser.id,
-                },
-            },
-            include: {
-                users: true,
-                messages: {
-                    include: {
-                        sender: true,
-                        seen: true,
-                    },
-                },
-            },
-        });
+  try {
+    const conversations = await prisma.conversation.findMany({
+      orderBy: {
+        lastMessageAt: "desc",
+      },
+      where: {
+        userIds: {
+          has: currentUser.id,
+        },
+      },
+      include: {
+        users: true,
+        messages: {
+          include: {
+            sender: true,
+            seen: true,
+          },
+        },
+      },
+    });
 
-        return conversations;
-    } catch (error: any) {
-        return [];
-    }
+    return conversations;
+  } catch (error: any) {
+    return [];
+  }
 };
 
 export default getConversations;
--- app/actions/getUsers.ts
+++ app/actions/getUsers.ts
@@ -1,30 +1,30 @@
-import prismaDB from "@/app/libs/prismadb";
+import prismaDB from "@/app/lib/prismadb";
 import getSession from "./getSession";
 
 const getUsers = async () => {
-    const session = await getSession();
+  const session = await getSession();
 
-    if (!session?.user?.email) {
-        return [];
-    }
+  if (!session?.user?.email) {
+    return [];
+  }
 
-    try {
-        const users = await prismaDB.user.findMany({
-            orderBy: {
-                createdAt: "desc",
-            },
-            where: {
-                NOT: {
-                    email: session.user.email,
-                },
-            },
-        });
+  try {
+    const users = await prismaDB.user.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+      where: {
+        NOT: {
+          email: session.user.email,
+        },
+      },
+    });
 
-        return users;
-    } catch (error: any) {
-        console.log("GET_USER_UERROR", error);
-        return [];
-    }
+    return users;
+  } catch (error: any) {
+    console.log("GET_USER_UERROR", error);
+    return [];
+  }
 };
 
 export default getUsers;
