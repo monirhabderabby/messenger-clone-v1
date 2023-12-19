e0416fa365acfde58908a4661354ae91c34a2335
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2023-12-19 10:06:55 +0600

    fsd

--- app/actions/getConversationById.ts
+++ app/actions/getConversationById.ts
@@ -1,28 +1,28 @@
-import prismaDB from "../libs/prismadb";
+import prismaDB from "@/app/lib/prismadb";
 import getCurrentUser from "./getCurrentUser";
 
 const getConversationById = async (conversationId: string) => {
-    try {
-        const currentUser = await getCurrentUser();
+  try {
+    const currentUser = await getCurrentUser();
 
-        if (!currentUser) return null;
+    if (!currentUser) return null;
 
-        const conversation = await prismaDB.conversation.findUnique({
-            where: {
-                id: conversationId,
-            },
-            include: {
-                users: true,
-            },
-        });
+    const conversation = await prismaDB.conversation.findUnique({
+      where: {
+        id: conversationId,
+      },
+      include: {
+        users: true,
+      },
+    });
 
-        if (!conversation) return null;
+    if (!conversation) return null;
 
-        return conversation;
-    } catch (error: any) {
-        console.log("[getConversationById_Error]", error);
-        return null;
-    }
+    return conversation;
+  } catch (error: any) {
+    console.log("[getConversationById_Error]", error);
+    return null;
+  }
 };
 
 export default getConversationById;
